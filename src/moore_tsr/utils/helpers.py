b1c94b6bf78ffbf10ddfeac2c1c01ee2373c4afe
Salif SAWADOGO <96554481+sawadogosalif@users.noreply.github.com>
2025-03-14 17:12:17 +0100

    Update helpers.py

--- src/moore_tsr/utils/helpers.py
+++ src/moore_tsr/utils/helpers.py
@@ -2,6 +2,8 @@ import gc
 import torch
 import random
 import numpy as np
+from loguru import logger 
+from transformers import PreTrainedModel, PreTrainedTokenizer
 
 def cleanup():
     """
@@ -11,12 +13,9 @@ def cleanup():
     if torch.cuda.is_available():
         torch.cuda.empty_cache()
 
-def set_random_seed(seed: int = 42):
+def set_random_seed(seed: int = 2025):
     """
     Définit la seed aléatoire pour assurer la reproductibilité.
-
-    Args:
-        seed (int): seed aléatoire.
     """
     random.seed(seed)
     np.random.seed(seed)
@@ -38,3 +37,39 @@ def format_time(seconds: float) -> str:
     minutes = int((seconds % 3600) // 60)
     seconds = int(seconds % 60)
     return f"{hours}h {minutes}m {seconds}s"
+
+def get_batch_pairs(data, batch_size):
+    """Translation bidirectionelle  
+    Sélectionne un couple de langues au hasard et retourne un batch de textes source et cible.
+    """
+    
+    language_pairs = [('french', "fra_Latn"), ('moore', "moor_Latn")]
+    (src_lang, src_lang_code), (tgt_lang, tgt_lang_code) = random.sample(language_pairs, 2)
+
+    src_texts, tgt_texts = [], []
+    for _ in range(batch_size):
+        item = data.iloc[random.randint(0, len(data) - 1)]
+        src_texts.append(item[src_lang])  # Texte source
+        tgt_texts.append(item[tgt_lang])  # Texte cible
+
+    return src_texts, tgt_texts, src_lang_code, tgt_lang_code
+
+    
+def cleanup():
+    """Clear memory to prevent CUDA out-of-memory errors."""
+    gc.collect()
+    torch.cuda.empty_cache()
+
+def save_model_and_tokenizer(model: PreTrainedModel, tokenizer: PreTrainedTokenizer, path: str, drive_path: str = None):
+    """
+    Sauvegarde le modèle et le tokenizer à l'emplacement spécifié.
+    Si un chemin Google Drive est fourni, il sauvegarde aussi une copie sur Google Drive.
+    """
+    logger.info(f"Saving model to {path}")
+    model.save_pretrained(path)
+    tokenizer.save_pretrained(path)
+    
+    if drive_path:
+        logger.info(f"Saving model to Google Drive at {drive_path}")
+        model.save_pretrained(drive_path)
+        tokenizer.save_pretrained(drive_path)
