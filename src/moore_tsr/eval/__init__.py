9885416ae75766612f1beefe18dc923530d165ff
Salif SAWADOGO <96554481+sawadogosalif@users.noreply.github.com>
2025-03-14 17:37:33 +0100

    Update __init__.py

--- src/moore_tsr/eval/__init__.py
+++ src/moore_tsr/eval/__init__.py
@@ -1,9 +1,3 @@
-from .metrics import calculate_bleu_scores, evaluate_model_with_bleu
+from .metrics import calculate_bleu_scores, evaluate_model_with_bleu, evaluate_model_loss
 from .qualitative import evaluate_qualitative, display_translation_examples, create_loss_visualization
 
-__all__ = [
-    "calculate_bleu_scores",
-    "evaluate_model_with_bleu",
-    "evaluate_qualitative",
-    "display_translation_examples"
-]
